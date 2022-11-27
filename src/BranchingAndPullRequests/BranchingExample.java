c2379af9d6bc7e6517261875036a0fa830e45ee0
IlyaKim1 <ilyakim890@gmail.com>
2022-11-27 15:15:04 -0600

    Added thye hi m,ethod

--- src/BranchingAndPullRequests/BranchingExample.java
+++ src/BranchingAndPullRequests/BranchingExample.java
@@ -4,4 +4,8 @@ public class BranchingExample {
     public static void main(String[] args) {
         System.out.println("This is the Main code");
     }
+
+    static void hi(){
+        System.out.println("Hi, this is the first branch");
+    }
 }
